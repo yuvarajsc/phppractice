e047f93951b5f85ef91d98f6c39160fecb0b0c44
yuvarajsc <97349861+yuvarajsc@users.noreply.github.com>
2022-12-05 10:48:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
+echo "<h2>PHP VER1</h2>";
+echo "Hello Yuvaraj!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?>
